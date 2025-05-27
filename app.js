5d87e3cdce88425e9136dbfff95c51efee5a8317
Fatuma Abdi <fatumaabdiibra81@gmail.com>
2025-05-27 10:26:34 +0300

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-import { logVisit } from './rewards.js';
-
-window.checkIn = async function () {
-  const phone = document.getElementById('phone').value;
-  if (!phone) return alert('Please enter a phone number.');
-  const response = await logVisit(phone);
-  document.getElementById('result').textContent = response.message;
-}
